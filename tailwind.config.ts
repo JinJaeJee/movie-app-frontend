d4530183212ebdca9bdba0dcfcd10be380a1b44c
JinJeaJee <jpluemphiromnat@gmail.com>
2024-04-06 19:57:14 +0700

    feat: hello react im back

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+export default config;
