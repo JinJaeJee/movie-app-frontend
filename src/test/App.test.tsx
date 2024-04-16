56a18b8651b58c7188b9866e7c5656340a059750
JinJeaJee <jpluemphiromnat@gmail.com>
2024-04-16 15:22:33 +0700

    feat add sorted test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -48,4 +48,79 @@ describe("Home Component", () => {
     fireEvent.click(getByText("Movie 1"));
     expect(screen.getByText("Description 1")).toBeInTheDocument();
   });
+
+  it("Sorts movie by release year", async () => {
+    const movies = [
+      {
+        _id: "1",
+        id: 1,
+        title: "Movie 1",
+        genre: ["Action", "Adventure"],
+        releaseYear: 2020,
+        description: "Description 1",
+        thumbnailUrl: "thumbnail1.jpg",
+        rating: 4.5,
+        movieId: "movie1",
+      },
+      {
+        _id: "2",
+        id: 2,
+        title: "Movie 2",
+        genre: ["Comedy"],
+        releaseYear: 2018,
+        description: "Description 2",
+        thumbnailUrl: "thumbnail2.jpg",
+        rating: 3.8,
+        movieId: "movie2",
+      },
+      {
+        _id: "3",
+        id: 3,
+        title: "Movie 3",
+        genre: ["Action", "Adventure"],
+        releaseYear: 2020,
+        description: "Description 3",
+        thumbnailUrl: "thumbnail3.jpg",
+        rating: 5.5,
+        movieId: "movie3",
+      },
+      {
+        _id: "4",
+        id: 4,
+        title: "Movie 4",
+        genre: ["Comedy"],
+        releaseYear: 2018,
+        description: "Description 4",
+        thumbnailUrl: "thumbnail4.jpg",
+        rating: 6.8,
+        movieId: "movie4",
+      },
+    ];
+
+    mockedAxios.get.mockResolvedValueOnce({ data: movies });
+
+    const { getByLabelText, getByText, getAllByText } = render(<Home />);
+
+    await waitFor(() => {
+      // Check if Movie 1 and Movie 2 are in the document
+      expect(getByText("Movie 1")).toBeInTheDocument();
+      expect(getByText("Movie 2")).toBeInTheDocument();
+    });
+    const sortByYearDropdown = getByLabelText(
+      "Sort by Year:"
+    ) as HTMLSelectElement;
+
+    fireEvent.change(sortByYearDropdown, { target: { value: "ascending" } });
+    await waitFor(() => {
+      // Check if Movie 1 comes before Movie 2 (ascending order)
+      const movieTitles = getAllByText(/Movie \d/);
+      const movie1Index = movieTitles.findIndex(
+        (element) => element.textContent === "Movie 1"
+      );
+      const movie2Index = movieTitles.findIndex(
+        (element) => element.textContent === "Movie 2"
+      );
+      expect(movie2Index).toBeLessThan(movie1Index);
+    });
+  });
 });
